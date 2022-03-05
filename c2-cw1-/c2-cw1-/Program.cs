6ea417a08b1f9c3389842127962af0c685dae7c8
saraallahdan-C43 <66418277+saraallahdan-C43@users.noreply.github.com>
2022-03-05 14:10:13 +0300

    c2-cw1

---
+++ c2-cw1-/c2-cw1-/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace c2_cw1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charecterName = "Mario";
+            string superPower1 = "double cherry";
+            string superPower2 = "cannon box";
+            int age = 60;
+            int speed = 120;
+            int height = 140;
+            Console.WriteLine("Hello World");
+            Console.WriteLine("My name is {0} ,My  super powers are {1} and {2} , My age {3} , my speed {4}", charecterName, superPower1, superPower2, age, speed);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
